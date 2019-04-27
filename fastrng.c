28cc48e87606b006fc8ab097247f121d6a26b724
Brett Dong <brett.browning.dong@gmail.com>
2019-04-27 21:25:02 +0800

    Wrote wrapper for RNG

--- fastrng.c
+++ fastrng.c
@@ -1,5 +1,7 @@
 #include "fastrng.h"
+#include <time.h>
 uint64_t state;
+int initialize;
 
 void init_rng(const uint32_t seed)
 {
@@ -10,7 +12,7 @@ void init_rng(const uint32_t seed)
 // Xorshift* random number generator
 // Implementation from https://en.wikipedia.org/wiki/Xorshift
 #define XORSHIFT_MAGIC 0x2545F4914F6CDD1DULL;
-uint64_t rng(uint64_t *state)
+uint64_t xorshift_rng(uint64_t *state)
 {
     uint64_t x = *state;
     x ^= x >> 12;
@@ -18,4 +20,14 @@ uint64_t rng(uint64_t *state)
     x ^= x >> 27;
     *state = x;
     return x * XORSHIFT_MAGIC;
+}
+
+uint64_t rng()
+{
+    if(initialize == 0)
+    {
+        init_rng(time(NULL));
+        initialize = 1;
+    }
+    return xorshift_rng(&state);
 }
